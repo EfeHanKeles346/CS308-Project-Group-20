13a6d293a4769c6e93a9411f1e19a4679b39a701
Efe Han Keleş <efe.keles@sabanciuniv.edu>
2026-03-19 16:34:17 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// ======================== TechMind E-Commerce ========================
+// Main JavaScript file - features will be added via feature branches
+
+console.log("TechMind Store initialized");
